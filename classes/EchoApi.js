a8f3d111ff35db671b9844113268114a4d0126a5
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-04-18 15:20:17 +0200

    Update EchoApi.js

--- classes/EchoApi.js
+++ classes/EchoApi.js
@@ -1,18 +1,24 @@
 class EchoApi {
-    constructor(baseUrl) {
+    constructor(baseUrl = "https://echo.kuricki.com/api/") {
         this.baseUrl = baseUrl;
     }
 
-    async request(endpoint, method, data, token) {
-        const response = await fetch(`${this.baseUrl}/${endpoint}`, {
-            method,
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": token
-            },
-            body: JSON.stringify(data)
-        });
+    async call(token, path, method = 'GET', body = null, forceString = true, forceContentType = false) {
+        console.log("[API] - ", method, " - ", path, " - ", body)
 
-        return response.json();
+        const options = {
+            method: method,
+            cache: 'no-cache',
+            cors: false,
+            headers: new Headers({
+              'Content-Type': forceContentType ? null : 'application/json',
+              "Authorization": token
+            }),
+            body: body ? (forceString ? JSON.stringify(body) : body) : null
+          };
+
+        return await fetch(this.baseUrl + path, options);
     }
 }
+
+module.exports = EchoApi;
